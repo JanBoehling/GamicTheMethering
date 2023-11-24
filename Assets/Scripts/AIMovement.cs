e3f53cdd21a1f2bc5d027db43f9c20a2116417c9
InspectorRaupe <jonas.kluge03@gmail.com>
2023-11-24 16:58:19 +0100

    Made it that the Zombie stands still every 5 seconds;
    We can still change that to more or less seconds and make it random, this is just the simple version i went for.

--- Assets/Scripts/AIMovement.cs
+++ Assets/Scripts/AIMovement.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -7,10 +9,38 @@ public class AIMovement : MonoBehaviour
 {
     [SerializeField] NavMeshAgent agentZombie;
     [SerializeField] Transform player;
+    float timeToStop = 5f;
+    float maxTimeInterval = 5f;
 
+    private void Start()
+    {
+        agentZombie.SetDestination(player.position);
+    }
 
     private void Update()
     {
-        agentZombie.SetDestination(player.position);
+        timeToStop -= Time.deltaTime;
+        
+        Debug.Log(timeToStop);
+
+        if (timeToStop <= 3)
+        {
+            Debug.Log("Time Reached Zero");
+            StopMoving();
+        }
+    }
+
+    private void StopMoving()
+    {
+            agentZombie.isStopped = true;
+            Debug.Log("StayingStill:" + timeToStop);
+
+            if (timeToStop <= 0)
+            {
+                Debug.Log("End freezing");
+                timeToStop = maxTimeInterval;
+                agentZombie.isStopped = false;
+            }
+        
     }
 }

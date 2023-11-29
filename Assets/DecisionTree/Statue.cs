107e9176b96b18a04eee04b1d5299d447b344574
InspectorRaupe <jonas.kluge03@gmail.com>
2023-11-29 14:17:36 +0100

    Statue Behavior;

--- Assets/DecisionTree/Statue.cs
+++ Assets/DecisionTree/Statue.cs
@@ -1,16 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class Statue : MonoBehaviour
 {
+    [SerializeField] NavMeshAgent statueAgent;
+
     public void IsVisible()
     {
-
+        statueAgent.isStopped = true;
     }
 
     public void IsInvisible() 
     {
-        
+        statueAgent.isStopped= false;
     }
 }
